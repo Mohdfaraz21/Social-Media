fc79628706b1175f3b7c3e359ca98501169e3b5f
Mohdfaraz21 <mofaraz7800@gmail.com>
2025-08-13 23:59:23 +0530

    basic of routes setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,38 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const usersRoute = require("./routes/users");
+
 const app = express();
+dotenv.config();
 
+// @MongoDB connection (async/await)
+(async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("✅ Connected to Database");
+    } catch (err) {
+        console.error("❌ MongoDB connection error:", err);
+        
+    }
+})();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
-    
+//@Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+//@Routes
+app.use("/api/v1/users", usersRoute);
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
+
